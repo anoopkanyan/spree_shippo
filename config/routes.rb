541a2b8912db2a3169961ac30a353c607fe2d854
Simon Kreuz <simon@goshippo.com>
2014-10-04 22:47:29 -0700

    Improved routes & paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Spree::Core::Engine.routes.draw do
 	namespace :admin do
-		resources :shippo
+		get 'shippo/settings', :to => 'shippo#show', :as => "shippo_settings"
+		get 'shippo/orders', :to => 'shippo#view_orders', :as => "shippo_orders"
+		get 'shippo/order/:id', :to => 'shippo#view_order', :as => "shippo_order"
   	end
 end
