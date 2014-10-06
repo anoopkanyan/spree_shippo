256ddfb48fd097a8c68e057b959aa404bc0acdef
Simon Kreuz <simon@goshippo.com>
2014-10-05 22:12:46 -0700

    Remove order list connect button

--- app/overrides/orders_index.rb
+++ app/overrides/orders_index.rb
@@ -6,12 +6,3 @@ Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
       <%= button_link_to Spree.t(:view_on_shippo), admin_shippo_orders_path, :icon => '' %>
     </li>
   ")
-
-Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
-  :name => 'add_shippo_connect_button_to_orders_page',
-  :insert_after => "erb[silent]:contains('content_for :page_actions')",
-  :text => "
-    <li>
-      <%= button_link_to Spree.t(:connect_shippo), admin_shippo_settings_path, :icon => '' %>
-    </li>
-  ")
