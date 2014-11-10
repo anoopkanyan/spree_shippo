397c17997422849ad94f99eabb57391f6dc264cf
Simon Kreuz <simonkreuz@me.com>
2014-11-10 19:12:42 +0100

    Use logged in user's email

--- app/controllers/spree/admin/shippo_controller.rb
+++ app/controllers/spree/admin/shippo_controller.rb
@@ -2,7 +2,6 @@ class Spree::Admin::ShippoController < Spree::Admin::BaseController
     include SpreeShippoLabels
 
     def show
-        @user = SpreeShippoLabels.get_shippo_user
         @shippo_connect_endpoint = SpreeShippoLabels.get_auth_url
         @store_url = SpreeShippoLabels.get_store_url
         @store_name = SpreeShippoLabels.get_store_name
